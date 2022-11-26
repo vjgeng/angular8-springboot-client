14d469f10f526fe6153b9c4eb869e6b1e931848a
Vatchapol Jittipattanakulchai <Vatchapol.Jittipattanakulchai@refinitiv.com>
2022-11-26 17:25:42 +0700

    first push angular client8

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:1.17.1-alpine
+COPY /dist/angular8-springboot-client /usr/share/nginx/html
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add', component: CreateEmployeeComponent },
+  { path: 'update/:id', component: UpdateEmployeeComponent },
+  { path: 'details/:id', component: EmployeeDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
